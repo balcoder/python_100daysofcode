981499d5330ff9b2192570c8f11a25ed54a87bdd
balcoder <des.barrett@gmail.com>
2023-09-11 18:39:54 +0100

    day35 done-- weather app with sms

---
+++ day35/list_practice.py
@@ -0,0 +1,4 @@
+list1 = [1, 2, 3]
+list2 = [10, 20, 30]
+pairs = [(x, y) for x in list1 for y in list2]
+print(pairs)
--- day35/main.py
+++ day35/main.py
@@ -1,3 +1,49 @@
-from api_key import api_key
+''' Get weather data from openweathermap.org '''
+import requests
+from twilio.rest import Client
+from api_key import API_KEY, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
 
-print(api_key)
+BASE_URL = "https://api.openweathermap.org/data/2.5/onecall?"
+weather_params = {
+    "lat":"51.683",
+    "lon":"-8.594",
+    "exclude":"current,minutely,daily",
+    "units": "metric",
+    "appid":API_KEY
+}
+
+# get json data from openweathermap.org
+response = requests.get(BASE_URL, params=weather_params, timeout=10)
+response.raise_for_status()
+data = response.json()
+
+will_rain = False
+
+def bring_umbrella(data):
+    ''' loop through weather data and send sms if it's going to rain '''
+    global will_rain
+    weather_for_12 =  data['hourly'][:12]
+
+    for hour in weather_for_12:
+        weather_condition_id = hour['weather'][0]['id']
+        if int(weather_condition_id < 700):
+            will_rain = True
+    if will_rain:
+        send_sms("It's going to rain. Remember to bring an ☂️")
+    else:
+        print('Enjoy the sun')
+
+def send_sms(message):
+    ''' use twilio to send sms text message '''
+    account_sid = TWILIO_ACCOUNT_SID
+    auth_token = TWILIO_AUTH_TOKEN
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='+15627413568',
+        body=message,
+        to='+353866067654'
+        )
+    print(message.status)
+
+bring_umbrella(data)
